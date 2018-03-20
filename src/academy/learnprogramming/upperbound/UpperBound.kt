7168a584e77b7e42df8f969be2960a4f57271a74
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-20 13:58:42 +0800

    Initial commit

---
+++ src/academy/learnprogramming/upperbound/UpperBound.kt
@@ -0,0 +1,11 @@
+package academy.learnprogramming.upperbound
+
+
+fun main(args: Array<String>) {
+
+    val ints = listOf(1, 2, 3, 4, 5)
+    val shorts: List<Short> = listOf(10, 20, 30 , 40)
+
+
+    
+}
